639b4ddcd704478dfa1e511eb76734142f4e486f
ibayetto@gmail.com <ibayetto@gmail.com@d389a50f-6534-611c-1d62-9b16620a17f7>
2012-09-13 23:39:01 +0000

    Subo el menú del tp.

---
+++ TpDatos/src/Constantes.h
@@ -0,0 +1,47 @@
+/*
+ * Constantes.h
+ *
+ *  Created on: 13/09/2012
+ *      Author: nacho
+ */
+
+#ifndef CONSTANTES_H_
+#define CONSTANTES_H_
+
+#define MP "MENU PRINCIPAL"
+#define MP_OP1 "1) Operaciones sobre Diccionario"
+#define MP_OP2 "2) Organización de Archivo"
+#define MP_OP3 "3) Componente de Estadísticas"
+
+#define SALIR "Salir"
+#define INGRESO "Por favor, ingresar una opción: "
+
+#define MD "MENU DICCIONARIO"
+#define MD_OP1 "1) Normalizar Diccionario"
+#define MD_OP2 "2) Aleatorizar Diccionario"
+
+#define MOAA "MENU ORGANIZACION DE ARCHIVO"
+#define MOAA_OP1 "1) Crear Árbol B+"
+#define MOAA_OP2 "2) Operaciones sobre Árbol B+"
+#define MOAA_OP3 "3) Operaciones Dispersión Extensible"
+
+#define MA "MENU ARBOL B+"
+#define MA_OP1 "1) Buscar término"
+#define MA_OP2 "2) Reportar contenido del Árbol B+"
+
+#define MDE "MENU DISPERSION EXTENSIBLE"
+#define MDE_OP1 "1) Incorporar un registro"
+#define MDE_OP2 "2) Eliminar un registro con identificador n"
+#define MDE_OP3 "3) Modificar un registro"
+#define MDE_OP4 "4) Listar contenido del archivo"
+
+#define MCE "MENU COMPONENTE ESTADISTICAS"
+#define MCE_OP1 "1) Cantidad de términos promedio por frase"
+#define MCE_OP2 "2) Tasa de fallos de términos"
+#define MCE_OP3 "3) Lista de términos no encontrados"
+#define MCE_OP4 "4) Ranking de los N términos más populares"
+
+#define ERR_RANGO "ERROR: Ingrese un número válido"
+#define INF_ERR_RANGO "Usted ingresó: "
+
+#endif /* CONSTANTES_H_ */
--- TpDatos/src/TpDatos.cpp
+++ TpDatos/src/TpDatos.cpp
@@ -1,16 +1,207 @@
-//============================================================================
-// Name        : TpDatos.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include "Constantes.h"
+#include "stdio.h"
+#include "stdlib.h"
+
 using namespace std;
 
+void menuDiccionario(){
+
+	int op;
+	bool ejecutando = true;
+
+	while(ejecutando){
+
+		cout << " " << endl;
+		cout << MD << endl;
+		cout << INGRESO << endl;
+		cout << MD_OP1 << endl;
+		cout << MD_OP2 << endl;
+		cout << "3) " << SALIR << endl;
+
+		cin >> op;
+
+		while(op<1 || op>3){
+			cout<< ERR_RANGO << INF_ERR_RANGO << op << endl;
+			cout << INGRESO << endl;
+			cin>> op;
+		}
+
+		switch(op){
+			case 1: cout << "TODO: " << MD_OP1 << endl;break;
+			case 2: cout << "TODO: " << MD_OP2 << endl;break;
+			case 3: ejecutando = false;break;
+			default: /*no hago nada*/;
+		}
+	}
+}
+
+void menuOperacionesArbol(){
+
+	int op;
+	bool ejecutando = true;
+
+	while(ejecutando){
+
+		cout << " " << endl;
+		cout << MA << endl;
+		cout << INGRESO << endl;
+		cout << MA_OP1 << endl;
+		cout << MA_OP2 << endl;
+		cout << "3) " << SALIR << endl;
+
+		cin >> op;
+
+		while(op<1 || op>3){
+			cout<< ERR_RANGO << INF_ERR_RANGO << op << endl;
+			cout << INGRESO << endl;
+			cin>> op;
+		}
+
+		switch(op){
+			case 1: cout << "TODO: " << MA_OP1 << endl;break;
+			case 2: cout << "TODO: " << MA_OP2 << endl;break;
+			case 3: ejecutando = false;break;
+			default: /*no hago nada*/;break;
+		}
+	}
+}
+
+void menuOperacionesDispersionExtensible(){
+
+	int op;
+	bool ejecutando = true;
+
+	while(ejecutando){
+
+		cout << " " << endl;
+		cout << MDE << endl;
+		cout << INGRESO << endl;
+		cout << MDE_OP1 << endl;
+		cout << MDE_OP2 << endl;
+		cout << MDE_OP3 << endl;
+		cout << MDE_OP4 << endl;
+		cout << "5) " << SALIR << endl;
+
+		cin >> op;
+
+		while(op<1 || op>5){
+			cout<< ERR_RANGO << INF_ERR_RANGO << op << endl;
+			cout << INGRESO << endl;
+			cin>> op;
+		}
+
+		switch(op){
+			case 1: cout << "TODO: " << MDE_OP1 << endl;break;
+			case 2: cout << "TODO: " << MDE_OP2 << endl;break;
+			case 3: cout << "TODO: " << MDE_OP3 << endl;break;
+			case 4: cout << "TODO: " << MDE_OP4 << endl;break;
+			case 5: ejecutando = false;break;
+			default: /*no hago nada*/;break;
+		}
+	}
+}
+
+void menuOrganizacionArchivo(){
+
+	int op;
+	bool ejecutando = true;
+
+	while(ejecutando){
+
+		cout << " " << endl;
+		cout << MOAA << endl;
+		cout << INGRESO << endl;
+		cout << MOAA_OP1 << endl;
+		cout << MOAA_OP2 << endl;
+		cout << MOAA_OP3 << endl;
+		cout << "4) " << SALIR << endl;
+
+		cin >> op;
+
+		while(op<1 || op>4){
+			cout<< ERR_RANGO << INF_ERR_RANGO << op << endl;
+			cout << INGRESO << endl;
+			cin>> op;
+		}
+
+		switch(op){
+			case 1: cout << "TODO: " << MOAA_OP1 << endl;break;
+			case 2: menuOperacionesArbol();break;
+			case 3: menuOperacionesDispersionExtensible();break;
+			case 4: ejecutando = false;break;
+			default: /*no hago nada*/;break;
+		}
+	}
+}
+
+void menuComponenteEstadisticas(){
+
+	int op;
+	bool ejecutando = true;
+
+	while(ejecutando){
+
+		cout << " " << endl;
+		cout << MCE << endl;
+		cout << INGRESO << endl;
+		cout << MCE_OP1 << endl;
+		cout << MCE_OP2 << endl;
+		cout << MCE_OP3 << endl;
+		cout << MCE_OP4 << endl;
+		cout << "5) " << SALIR << endl;
+
+		cin >> op;
+
+		while(op<1 || op>5){
+			cout<< ERR_RANGO << INF_ERR_RANGO << op << endl;
+			cout << INGRESO << endl;
+			cin>> op;
+		}
+
+		switch(op){
+			case 1: cout << "TODO: " << MCE_OP1 << endl;break;
+			case 2: cout << "TODO: " << MCE_OP2 << endl;break;
+			case 3: cout << "TODO: " << MCE_OP3 << endl;break;
+			case 4: cout << "TODO: " << MCE_OP4 << endl;break;
+			case 5: ejecutando = false;break;
+			default: /*no hago nada*/;break;
+		}
+	}
+}
+
 int main() {
-	cout << "!!!Soy Nacho!!!, soy Fran, prueba desde eclipse!!" << endl;
-	cout << "!!!Hello World!!!" << endl;
+
+	int op;
+	bool ejecutando = true;
+
+	while(ejecutando){
+
+		cout << " " << endl;
+		cout << MP << endl;
+		cout << INGRESO << endl;
+		cout << MP_OP1 << endl;
+		cout << MP_OP2 << endl;
+		cout << MP_OP3 << endl;
+		cout << "4) " << SALIR << endl;
+
+		cin >> op;
+
+		while(op<1 || op>4){
+			cout<< ERR_RANGO << INF_ERR_RANGO << op << endl;
+			cout << INGRESO << endl;
+			cin>> op;
+		}
+
+		switch(op){
+			case 1: menuDiccionario();break;
+			case 2: menuOrganizacionArchivo();break;
+			case 3: menuComponenteEstadisticas();break;
+			case 4: ejecutando = false;break;
+			default: /*no hago nada*/;break;
+		}
+
+	}
+
 	return 0;
 }
