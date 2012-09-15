da220fa6608004fae59597bf48af9cee58892bef
francisco.disalvo90@gmail.com <francisco.disalvo90@gmail.com@d389a50f-6534-611c-1d62-9b16620a17f7>
2012-09-15 18:38:16 +0000

    Agregue el normalizador y todo anda bien, dps si quieren prueben cada uno a ver si falla por alguna config distinta.

---
+++ trunk/TpDatos/src/Normalizador.h
@@ -0,0 +1,32 @@
+/*
+ * Normalizador.h
+ *
+ *  Created on: 12/09/2012
+ *      Author: francisco
+ */
+
+#ifndef NORMALIZADOR_H_
+#define NORMALIZADOR_H_
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+class Normalizador {
+	private:
+
+	public:
+		Normalizador();
+		virtual ~Normalizador();
+		void normalizarArchivo(char rutaEntrada[]);
+		void cortarPalabra(string *unaPalabra);
+		void sacarAcentos(string *unaPalabra);
+		void sacarMayusculas(string *unaPalabra);
+		void normalizarPalabra(string *unaPalabra);
+		int posicionCaracter(string unaPalabra, char L);
+		void cambiarLetra(string *unaPalabra, char L, int pos);
+};
+
+#endif /* NORMALIZADOR_H_ */
