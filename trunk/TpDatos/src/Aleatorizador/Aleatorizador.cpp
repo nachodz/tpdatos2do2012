bfbf5ee2defe85fa4efc0cd9e240ccef7f8e1267
ibayetto@gmail.com <ibayetto@gmail.com@d389a50f-6534-611c-1d62-9b16620a17f7>
2012-09-21 23:14:37 +0000

    Subo avance con Arbol y Estructuras Desharcodeo path de archivos en Normalizador y Aleatorizador

---
+++ trunk/TpDatos/src/Aleatorizador/Aleatorizador.cpp
@@ -0,0 +1,74 @@
+/*
+ * Aleatorizador.cpp
+
+ *
+ *  Created on: 15/09/2012
+ *      Author: francisco
+ */
+
+#include "Aleatorizador.h"
+
+Aleatorizador::Aleatorizador() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Aleatorizador::~Aleatorizador() {
+	// TODO Auto-generated destructor stub
+}
+
+void Aleatorizador::aleatorizarArchivo() {
+
+	srand(time(NULL));
+
+	char ruta[] = PATH_ARCHIVO_NORMALIZADO;
+
+	ifstream diccionarioNormalizado(ruta);
+
+	ofstream binario("diccionario.dat",ios::binary);
+
+	registro reg;
+
+	if(diccionarioNormalizado && binario){
+
+		diccionarioNormalizado>>reg.termino;
+
+		while (!diccionarioNormalizado.eof()){
+
+			reg.ID = 100000000+rand()%(200000001);
+			binario.write((char*)&reg,29);
+			diccionarioNormalizado>>reg.termino;
+		}
+
+		diccionarioNormalizado.close();
+		binario.close();
+		this->generarAchivoTabulado();
+		this->sortExterno();
+		cout<<"Archivo aleatorizado correctamente"<<endl;
+	}
+	else
+		cout<<"Error al aleatorizar el archivo";
+}
+
+void Aleatorizador::generarAchivoTabulado() {
+
+	ifstream entrada("diccionario.dat", ios::binary);
+	ofstream salida(PATH_ARCHIVO_TABULADO);
+
+	registro reg;
+
+	salida<<"DICCIONARIO TABULADO"<<endl;
+	salida<<" "<<endl;
+
+	entrada.read((char*)&reg,29);
+
+		while(!entrada.eof()){
+
+			salida<<reg.ID<<"  "<<reg.termino<<endl;
+			entrada.read((char*)&reg,29);
+		}
+}
+
+void Aleatorizador::sortExterno() {
+	cout<<"ordenamiento"<<endl;
+}
---
+++ trunk/TpDatos/src/Normalizador/Normalizador.cpp
@@ -0,0 +1,138 @@
+/*
+ * Normalizador.cpp
+ *
+ *  Created on: 12/09/2012
+ *      Author: francisco
+ */
+
+#include "Normalizador.h"
+
+
+
+Normalizador::Normalizador(){
+	// TODO Auto-generated constructor stub
+
+}
+
+Normalizador::~Normalizador(){
+	// TODO Auto-generated destructor stub
+}
+
+void Normalizador::normalizarArchivo() {
+
+	char rutaDic[] = PATH_DICCIONARIO, rutaNor[] = PATH_ARCHIVO_NORMALIZADO;
+
+	ifstream diccionario(rutaDic);
+	ofstream diccionarioNormalizado(rutaNor);
+
+	string palabraDiccionario;
+
+	if ((diccionario) && (diccionarioNormalizado)){
+
+		diccionario>>palabraDiccionario;
+		this->normalizarPalabra(&palabraDiccionario);
+
+		while (!diccionario.eof()) {
+
+			if (!(this->esStopword(palabraDiccionario))) {
+
+				diccionarioNormalizado<<palabraDiccionario<<endl;
+			}
+			diccionario>>palabraDiccionario;
+			this->normalizarPalabra(&palabraDiccionario);
+		}
+
+		diccionario.close();
+		diccionarioNormalizado.close();
+		cout<<"Diccionario normalizado correctamente"<<endl;
+	}
+	else
+		cout<<"error al normalizar el diccionario"<<endl;
+}
+
+void Normalizador::normalizarPalabra(string *unaPalabra){
+	this->cortarPalabra(unaPalabra);
+	this->sacarAcentos(unaPalabra);
+	this->sacarMayusculas(unaPalabra);
+}
+
+
+void Normalizador::cortarPalabra(string *unaPalabra){
+	int posicion;
+
+	posicion = this->posicionCaracter(*unaPalabra,'/');
+	unaPalabra->erase(posicion,unaPalabra->size());
+}
+
+void Normalizador::sacarAcentos(string *unaPalabra){
+	char letra;
+	int tamanioPalabra, posicion = 0;
+
+	tamanioPalabra = ((unaPalabra->size()) - 1);
+	while (posicion <= tamanioPalabra){
+		letra = unaPalabra->at(posicion);
+		this->cambiarLetra(unaPalabra,letra,posicion);
+		posicion++;
+	}
+}
+
+
+void Normalizador::sacarMayusculas(string *unaPalabra){
+	transform(unaPalabra->begin(), unaPalabra->end(), unaPalabra->begin(), (int( * )(int))std::tolower);
+}
+
+
+int Normalizador::posicionCaracter(string unaPalabra, char L){
+	int posicion = 0, tamanioPalabra = unaPalabra.size();
+	char letraExtraida;
+
+	letraExtraida = unaPalabra.at(posicion);
+	while ((letraExtraida != L) && (posicion != (tamanioPalabra - 1))){
+		posicion++;
+		letraExtraida = unaPalabra.at(posicion);
+	}
+	return posicion;
+}
+
+
+void Normalizador::cambiarLetra(string *unaPalabra, char L, int pos){
+	int ascii = L;
+	switch(ascii){
+	case(-31): case(-63): unaPalabra->operator [](pos) = 'a';
+					break;
+	case(-23): case(-55): unaPalabra->operator [](pos) = 'e';
+					break;
+	case(-19): case(-51): unaPalabra->operator [](pos) = 'i';
+					break;
+	case(-13): case(-45):unaPalabra->operator [](pos) = 'o';
+					break;
+	case(-6): case(-4): case(-38): case(-36): unaPalabra->operator [](pos) = 'u';
+					break;
+	default: break;
+	}
+}
+
+bool Normalizador::esStopword(string unaPalabra) {
+
+	ifstream words(PATH_STOPWORDS);
+
+	string stopword;
+
+	words >> stopword;
+
+	while(!words.eof()) {
+
+		if (stopword == unaPalabra){
+			return true;
+		}
+		else
+			words >> stopword;
+	}
+
+return false;
+}
+
+
+
+
+
