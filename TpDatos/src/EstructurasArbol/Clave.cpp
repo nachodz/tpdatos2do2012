537c06b29cae1347de0cce585627fcea424f5d0e
ibayetto@gmail.com <ibayetto@gmail.com@d389a50f-6534-611c-1d62-9b16620a17f7>
2012-09-21 23:31:37 +0000

    Subo avance con Arbol y Estructuras Desharcodeo path de archivos en Normalizador y Aleatorizador

---
+++ TpDatos/src/EstructurasArbol/Clave.cpp
@@ -0,0 +1,93 @@
+#include "Clave.h"
+
+using std::string;
+
+
+Clave::Clave()
+{
+	this->clave = "";
+}
+
+Clave::Clave(string clave)
+{
+	this->clave = clave;
+}
+
+string Clave::toString() const{
+	return this->clave;
+}
+
+/*
+ * Hay que guardar el tamaño tambien.
+ */
+Persistencia Clave::Serializar()
+{
+	Persistencia cadena;
+	char tamano = this->getTamanio();
+	cadena.agregarAlFinal(&tamano,TAM_LONG_CLAVE);
+	cadena.agregarAlFinal(this->clave);
+	return cadena;
+}
+
+bool Clave::Hidratar(Persistencia &cadena){
+	bool exito;
+	if (cadena.getTamanio() < TAM_LONG_CLAVE){
+		exito = false;
+	}else{
+		this->clave = "";
+		if(cadena.getTamanio() > TAM_LONG_CLAVE){
+			char tamano;
+			cadena.leer(&tamano,0,TAM_LONG_CLAVE);
+			int size = int(tamano);
+			char* buffer = (char*)malloc(size);
+			cadena.leer(buffer,TAM_LONG_CLAVE,size);
+			this->clave.append(buffer,size);
+			free(buffer);
+		}
+		exito = true;
+	}
+	return exito;
+}
+
+Clave* Clave::Clonar(){
+	return new Clave(this->clave);
+}
+
+int Clave::getTamanio(){
+	return (clave.size());
+}
+
+string Clave::getClave() const
+{
+	return clave;
+}
+
+void Clave::setClave(string clave)
+{
+    this->clave = clave;
+}
+
+Clave::~Clave() {
+
+}
+
+bool Clave::operator == (const Clave& otra) const{
+	return (this->clave == otra.clave);
+}
+
+bool Clave::operator != (const Clave& otra) const{
+	return (this->clave != otra.clave);
+}
+
+bool Clave::operator < (const Clave& otra) const{
+	return (this->clave < otra.clave);
+}
+
+bool Clave::operator > (const Clave& otra) const{
+	return (this->clave > otra.clave);
+}
+
+bool Clave::operator <= (const Clave& otra) const{
+	return (this->clave <= otra.clave);
+}
+
---
+++ TpDatos/src/EstructurasArbol/Clave.h
@@ -0,0 +1,85 @@
+
+#ifndef CLAVE_H_
+#define CLAVE_H_
+#include "../Persistencia/Serializable.h"
+#include "../Comunes/Constantes.h"
+#include <string.h>
+
+class Clave : public Serializable{
+
+private:
+	/*
+	 * La clave debera ser un string
+	 */
+	string clave;
+
+public:
+
+	/*
+	 * Constructor vacio
+	 */
+	Clave();
+
+	/*
+	 * Constructor
+	 */
+	Clave(string clave);
+
+	/*
+	 * Metodo que devuelve un string con los datos almacenados en el objeto (clave)
+	 * Pre:
+	 * Pos: string con los datos.
+	 */
+	string toString() const;
+
+	/*
+	 * Metodo que serializa la clave.
+	 * Se utiliza 1 char para representar el tamaño de la clave (0 a 255)
+	 * El resto de los bytes representan la propia clave.
+	 */
+	Persistencia Serializar();
+
+	/*
+	 * Metodo que hidrata la clave (heredado de Serializable)
+	 * Pre: la cadena de tipo CadenaBytes que se pasa por parametro, tiene que tener
+	 * 		al menos el tamanio de un char, utilizado para representar el tamaño de la clave.
+	 * Pos: Devuelve TRUE si pudo hidratar correctamente la cadena y la guarda
+	 * 		en el atributo clave. Si la cadena tiene una longitud invalida devuelve FALSE.
+	 */
+	bool Hidratar(Persistencia &cadena);
+
+	/*
+	 * Metodo que clona a la clave
+	 * Pre:-
+	 * Pos: Se retorna un puntero a un objeto de tipo clave, cuya memoria es alocada dinamicamente,
+	 * 		con los mismos valores que el actual.
+	 */
+	Clave* Clonar();
+
+//	 Getters y setters.
+	/*
+	 * Metodo que retorna el tamaño de la representacion de la clave
+	 * en bytes
+	 */
+	int getTamanio();
+	string getClave() const;
+	void setClave(string clave);
+
+	/*
+	 * Destructor:
+	 * Pos: libera la memoria de clave.
+	 */
+	virtual ~Clave();
+
+	bool operator != (const Clave& otra) const;
+
+	bool operator == (const Clave& otra) const;
+
+	bool operator < (const Clave& otra) const;
+
+	bool operator > (const Clave& otra) const;
+
+	bool operator <= (const Clave& otra) const;
+};
+
+#endif /* CLAVE_H_ */
