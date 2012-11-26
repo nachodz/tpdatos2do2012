e2b4e3bd18d2c13501abd2ae9e8b6f3b0171449e
damfinkel <damfinkel@d389a50f-6534-611c-1d62-9b16620a17f7>
2012-11-26 22:02:10 +0000

    metodo que recibe string de nroDocs en gamma y devuelve lista de nroDocs metodo que convierte tira de bits a string de 1s y 0s

--- trunk/Gamma/CodigoGamma.h
+++ trunk/Gamma/CodigoGamma.h
@@ -5,33 +5,35 @@
 #include <sstream>
 #include <stdlib.h>
 #include <list>
+#include <iostream>
+
 
 class CodigoGamma {
 
 private:
 	int numAConvertir;
 	/*
-	 * Método que me forma la primer parte de la conversion correspondiente al unario
+	 * MÃ©todo que me forma la primer parte de la conversion correspondiente al unario
 	 * pre: -
 	 * pos: Obtengo en un string el unario correspondiente
 	 */
 	std::string obtenerUnario(int numAConvertir);
 	/*
-	 * Método que me forma la segunda parte de la conversion correspondiente al binario
+	 * MÃ©todo que me forma la segunda parte de la conversion correspondiente al binario
 	 * pre: -
 	 * pos: Obtengo en un string el binario correspondiente
 	 */
 	std::string conversionRepresentacion(int numAConvertir);
 
 	/*
-	 * Método que me pasa a binario un numero en "cantidadBits" de  bits
+	 * MÃ©todo que me pasa a binario un numero en "cantidadBits" de  bits
 	 * pre: -
 	 * pos: Obtengo en un string el binario correspondiente
 	 */
 	std::string obtenerBinarioDeNBits(int valorConversion, int cantidadBits);
 
 	/*
-	 * Método que me pasa un binario a un numero entero
+	 * MÃ©todo que me pasa un binario a un numero entero
 	 * pre: -
 	 * pos: Obtengo el entero correspondiente al binario pasado por parametro
 	 */
@@ -42,19 +44,29 @@ public:
 	CodigoGamma(int numeroAConvertir);
 	virtual ~CodigoGamma();
 
-/*-----Método de conversion y de interpretacion Gamma-----*/
+/*-----MÃ©todo de conversion y de interpretacion Gamma-----*/
 	/*
-	 * Metodo que realiza la compresión a Gamma
+	 * Metodo que realiza la compresiÃ³n a Gamma
 	 * pre:-
 	 * pos: Conversion realizada y devuelta en un string
 	 */
 	std::string aplicarConversion();
 
 	/*
+	 * Realiza la conversion de un string en gamma que contiene cantDocs y los nrosDocs
+	 * y devuelve una lista de los nroDocs
+	 * pre: Le paso un string en gamma que tiene primero la cantDocs y despues los nroDocs
+	 * post: Devuelvo lista de nroDocs
+	 */
+
+	std::list<int> decodificarLista(std::string conversion);
+
+	/*
 	 * Metodo que realiza la conversion de Gamma a un numero
 	 * pre: Le paso una conversion hecha en gamma
 	 * pos: Obtengo el numero que representa esa conversion
 	 */
+
 	int interpretarConversion(std::string conversion);
 
 	/*
@@ -63,6 +75,17 @@ public:
 	 * pos: Obtengo la tira de bits en un string
 	 */
 	std::string comprimirLista(std::list<int> listaInvertida, int primero);
+
+	/*
+	 * Metodo que convierte una tira de bits a un string de unos y ceros
+	 * pre: Le paso un char*
+	 * post: devuelve el valor del char* en binario, en un string de unos y ceros
+	 */
+
+	std::string convertirAString(char* buf)
+
+
+
 /*------------ GETTERS AND SETTERS --------------*/
 	void setNumAConvertir (int num){
 		this->numAConvertir = num;
